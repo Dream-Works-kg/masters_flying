c9d4c7b8666df5328c73702c9c3635a00cfbd5af
Kadirov Muhammadaziz <126769438+KadirovKmck@users.noreply.github.com>
2025-03-14 09:18:24 +0600

    start

--- lib/src/core/navigator/app_router.dart
+++ lib/src/core/navigator/app_router.dart
@@ -1,17 +1,14 @@
 import 'package:go_router/go_router.dart';
+import 'package:masters_flying/src/presentations/home/views/home_view.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
     initialLocation: '/',
     routes: [
-      // GoRoute(
-      //   path: '/',
-      //   builder: (context, state) => SplashView(),
-      // ),
-      // GoRoute(
-      //   path: '/home_view',
-      //   builder: (context, state) => HomeView(),
-      // ),
+      GoRoute(
+        path: '/',
+        builder: (context, state) => HomeView(),
+      ),
     ],
   );
 }
---
+++ lib/src/presentations/home/views/home_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:masters_flying/src/core/core.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      body: Column(),
+    );
+  }
+}
